397f4480245fb30181fcc3d0b94f2a72ba252b79
Evgeny Samoylov <samoyl@gmail.com>
2024-06-12 18:55:40 +0200

    add Movies and Movie components

--- src/layout/App.jsx
+++ src/layout/App.jsx
@@ -1,11 +1,39 @@
+import { Component } from "react"
+import Movies from "./Movies"
 
-function App() {
+class App extends Component {
+  state = {
+    movies: [],
+  }
 
-  return (
-    <main className="container content">
-      Hello from movies app!
-    </main>
-  )
+  componentDidMount() {
+    fetch('http://www.omdbapi.com/?apikey=33602c1b&s=matrix')
+      .then(response => response.json())
+      .then(data => this.setState({movies: data.Search}))
+  }
+
+  render() {
+    const {movies} = this.state
+
+    return (
+      <main className="container content">
+        {
+          movies.length ? (
+            <Movies movies={movies} />
+          ) : <h5>Loading...</h5>
+        }
+      </main>
+    )
+  }
 }
+
+// function App() {
+
+//   return (
+//     <main className="container content">
+//         <Movies />
+//     </main>
+//   )
+// }
 
 export default App
